dac980743b12d65a93943df61c978ad7bdbef456
Fernando Nicolas <fersanti2896@gmail.com>
2022-01-24 23:59:58 -0600

    Evaluando si existen componentes en el componente GifGrid

--- src/test/components/GifGrid.test.js
+++ src/test/components/GifGrid.test.js
@@ -32,7 +32,10 @@ describe('Haciendo pruebas en componente GifGrid', () => {
         });
 
         const wrapper = shallow(<GifGrid category={category}/>);
+
         expect(wrapper).toMatchSnapshot();
+        expect(wrapper.find('p').exists()).toBe(false);
+        expect(wrapper.find('GifGridItem').length).toBe(gifs.length);
     });
     
 });
